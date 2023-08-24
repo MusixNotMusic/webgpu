9378b655c97a14fdd6873f3bb288b583ea3abdaa
musixnotmusic <279601306@qq.com>
2023-08-24 18:14:54 +0800

    ...

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,24 +1,21 @@
 <template>
     <el-row class="tac">
         <el-col :span="24">
-        <h3 class="m-l-4">WebGPU</h3>
+        <h2 class="m-l-2">WebGPU</h2>
             <el-menu default-active="2" class="el-menu-vertical-demo">
                 <tepmlate v-for="(menu, index) in navigationList">
                     <!-- node -->
                     <el-sub-menu v-if="menu.children" :index="index">
                         <template #title>
-                            <el-icon v-if="menu.icon"></el-icon>
                             <span>{{menu.name}}</span>
                         </template>
                         <el-menu-item v-for="(submenu, sIndex) in menu.children" :index="`${index}-${sIndex}`">
-                                <el-icon v-if="submenu.icon"></el-icon>
                                 <span><NuxtLink :to="submenu.path">{{submenu.name}}</NuxtLink></span>
                         </el-menu-item>
                     </el-sub-menu>
                     <!-- leaf -->
                     <el-menu-item v-else :index="index">
                         <template #title>
-                            <el-icon v-if="menu.icon"></el-icon>
                             <span><NuxtLink :to="menu.path">{{menu.name}}</NuxtLink></span>
                         </template>
                     </el-menu-item>
